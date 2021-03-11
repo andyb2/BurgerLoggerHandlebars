05f3f19bf7a2410eedbcfca0aac662ffc2400d1f
AMB <a.michael.boyle@gmail.com>
2021-03-11 09:23:11 -0500

    api routes all working

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const db = require("./app/config/connection")("burger_db", "rootroot");
 // for parsing incoming POST data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,6 +13,32 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   res.sendFile("./public/index.html");
+});
+
+app.get("/api/index", async (req, res) => {
+  let burgerList = await db.query(`SELECT * from burgers`);
+  res.send(burgerList);
+});
+
+app.post("/api/index", async (req, res) => {
+  const burgerInput = req.body;
+  let dbInput = await db.query(
+    `INSERT INTO burgers (title, eaten) VALUES ('${burgerInput.title}', 0)`
+  );
+  res.send(dbInput);
+});
+
+app.delete("/api/index/:id", async (req, res) => {
+  let id = req.params.id;
+  let dbDelete = await db.query(`DELETE FROM burgers WHERE id=${id};`);
+  res.send({ message: `Deleted ${id}` });
+});
+
+app.put("/api/index/:id", async (req, res) => {
+  let id = req.params.id;
+  console.log(id);
+  let updateDb = await db.query(`UPDATE burgers SET eaten=1 WHERE id=${id};`);
+  res.send({ message: `Updated ${id}` });
 });
 
 app.listen(PORT, function () {
